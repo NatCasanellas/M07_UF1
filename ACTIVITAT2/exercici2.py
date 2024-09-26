c3e0f71e022828922e16d4b2f6ac334ea360401a
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-09-26 08:59:14 +0200

    Afegit exercici 2

---
+++ ACTIVITAT2/exercici2.py
@@ -0,0 +1,10 @@
+#Exercici 2
+
+#Demanar a l’usuari que introdueixi un valor en €, 
+# seguidament demanar que introdueixi el IVA a 
+# aplicar-hi (4%, 10% o 21%) i finalment mostrar, 
+# per pantalla, el resultat del valor indicat 
+# per l’usuari, el % d’IVA demanat per l’usuari 
+# i el valor final amb l’IVA afegit. Si no afageix 
+# el IVA correcte, tornar a demanar el IVA fins que 
+# posi un dels 3 vàlids.
