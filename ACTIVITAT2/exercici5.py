bb8216b9ceaba653e2088cba144570af576c154a
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-09-30 18:49:46 +0200

    Exercici acabat

--- ACTIVITAT2/exercici5.py
+++ ACTIVITAT2/exercici5.py
@@ -1,2 +1,11 @@
 #Demanar a l’usuari un número. Indicar si el número 
 # inserit per l’usuari és parell o senar.
+
+#demanar un numero a l'usuari
+numero = int(input("Introdueix un numero: "))
+
+#comprovar si es un parell o senar
+if numero % 2 == 0:
+    print(f"El numero {numero} es parell")
+else:
+    print(f"El numero {numero} es senar")    
