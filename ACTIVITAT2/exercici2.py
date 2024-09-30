a9f989f6fd6f4e05df66f7c4042b1a6bf089305a
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-09-30 16:38:54 +0200

    Exercici acabat

--- ACTIVITAT2/exercici2.py
+++ ACTIVITAT2/exercici2.py
@@ -8,3 +8,24 @@
 # i el valor final amb l’IVA afegit. Si no afageix 
 # el IVA correcte, tornar a demanar el IVA fins que 
 # posi un dels 3 vàlids.
+
+#funcio per calcular l'iva
+def calcular_iva(valor, iva_percentatge):
+    iva = valor * iva_percentatge / 100
+    return valor + iva
+
+#Demana el valor a l'usuari
+valor = float(input("Introdueix un valor en €: "))
+
+#demana el % de l'iva 
+iva_parcentatge = None
+while iva_parcentatge not in [4, 10, 21]:
+    iva_parcentatge = int(input("Introdueix el % d'iva (4%, 10%, 21%):"))
+
+#Calcul del valor amb l'iva afegit
+valor_final = calcular_iva(valor, iva_percentatge)
+
+#Resultat
+print(f"Valor original: {valor}€")
+print(f"IVA aplicat: {iva_parcentatge}%")
+print(f"Valor final amb IVA: {valor_final}€")
