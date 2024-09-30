405efe275f8a36c4bae43fd22c61073a5b1af151
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-09-30 18:54:39 +0200

    Exercici 6 loops

---
+++ ACTIVITAT2/exercici6.py
@@ -0,0 +1,16 @@
+#Exercici de loops.
+#Mostrar els umeros de l'1 al 100 amb un while
+
+#WHILE
+#inicialitzar la variable
+i = 1
+
+#Bucle while per mostrar els numeros de l'1 al 100
+while i <= 100:
+    print(i)
+    i += 1 #incrementa la variable
+
+#FOR
+#Bucle fos per mostrar els numeros de l'1 al 100
+for i in range(1, 101):
+    print(i)    
