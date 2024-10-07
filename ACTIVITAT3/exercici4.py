ddb8b21eee37efe51966d0747f140ccc43614091
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-07 08:31:36 +0200

    Exercici 4

---
+++ ACTIVITAT3/exercici4.py
@@ -0,0 +1,6 @@
+'''
+Demanar a l’usuari que introdueixi 10 números separats per un espai. 
+Al acabar, el programa els introduirà en una tupla i els ordenarà 
+(major o menor, com volgueu), mostrant per pantalla el contingut 
+de la tupla.
+'''
