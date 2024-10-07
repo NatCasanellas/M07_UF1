283698a37219d24bcc69078054f4fdb6cfb3ba40
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-07 23:23:22 +0200

    Exercici 9

--- ACTIVITAT3/exercici9.py
+++ ACTIVITAT3/exercici9.py
@@ -7,3 +7,29 @@ el missatge A <assignatura> ha tret <nota>.
 Intenta millorar aquest programa utilitzant altres 
 tipus de dades explicats. Cal que l’exercici tingui les dues versions.
 '''
+#Llistat moduls
+moduls = ["Programacio", "Bases de dades", "Llenguatge de marques", "Disseny", "Entorn client", "Entorn servidor"]
+
+#Llista per emmagatzemar notes
+notes = []
+
+#
+for modul in moduls:
+    nota = float(input(f"Introdueix la nota del {modul}: "))
+    notes.append(nota)
+
+for i in range(len(moduls)):
+    print(f"A {moduls[i]} ha tret {notes[i]} ")    
+
+###########################################################################
+#diccionari moduls
+moduls_notes = {}
+
+moduls = ["Programacio", "Bases de dades", "Llenguatge de marques", "Disseny", "Entorn client", "Entorn servidor"]
+
+for modul in  moduls:
+    nota = float(input(f"Introdueix la nota del {modul}: "))
+    moduls_notes[modul] = nota
+
+for modul, nota in moduls_notes.items():
+    print(f"A {modul} ha tret {nota}")    
