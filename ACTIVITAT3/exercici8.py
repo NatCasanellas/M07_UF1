08991d60283ed9c6b53a9910803382d460ac6ece
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-07 20:25:46 +0200

    Exercici 8

--- ACTIVITAT3/exercici8.py
+++ ACTIVITAT3/exercici8.py
@@ -9,3 +9,21 @@ Números de l’usuari:
 suma total:
 mitjana:
 '''
+#Demanar usuari posi 10 numeros separats per espais
+intr_numeros = input("Introdueix 10 numeros separats per espais: ")
+
+#Afegir els numeros en una llista
+numeros = list(map(int, intr_numeros.split()))
+
+#Calcular la suma de tots els numeros i la seva mitjana
+suma_total = sum(numeros)
+mitjana = suma_total / len(numeros)
+
+#afageix la suma i la mitjana a la llista
+numeros.append(suma_total)
+numeros.append(mitjana)
+
+#Mostra per pantalla
+print("Numeros de l'usuari: ", numeros[:-2])
+print("Suma total: ", suma_total)
+print("Mitjana: ", mitjana)
