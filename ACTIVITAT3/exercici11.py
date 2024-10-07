2b78e95275430a469d22b6420ef1c068962b8c40
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-07 08:37:14 +0200

    Exercici 11

---
+++ ACTIVITAT3/exercici11.py
@@ -0,0 +1,6 @@
+'''
+Crear un diccionari de divises amb els seus símbols 
+(exemple: Euro:€…). Demana a l’usuari una divisa i 
+mostra el seu símbol. Si la divisa no està al diccionari 
+mostra un missatge.
+'''
