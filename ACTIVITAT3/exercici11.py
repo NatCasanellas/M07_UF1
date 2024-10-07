6e47b87ff57993fceab768ac2f37d135083d62df
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-07 23:23:11 +0200

    Exercici 11

--- ACTIVITAT3/exercici11.py
+++ ACTIVITAT3/exercici11.py
@@ -4,3 +4,21 @@ Crear un diccionari de divises amb els seus símbols
 mostra el seu símbol. Si la divisa no està al diccionari 
 mostra un missatge.
 '''
+
+#diccionari divises amb els seus simbols
+divises = {
+    "Euro": "€",
+    "Dolar": "$",
+    "Lliura": "£",
+    "Ien": "¥",
+    "Franc suis": "CHF",
+    "Dolar australia": "A$"
+}
+
+#demana al usuari que introdueixi una divisa
+divisa = input("Introdueix el nom de la divisa: ")
+
+if divisa in divises:
+    print(f"El simbol de {divisa} es {divises[divisa]}")
+else:
+    print(f"La divisa {divisa} no es troba al diccionari")    
