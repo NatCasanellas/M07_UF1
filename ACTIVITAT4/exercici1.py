a5f04fb089a09de7124808752bb46786de59e0ca
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-14 23:24:02 +0200

    Exercici 1

--- ACTIVITAT4/exercici1.py
+++ ACTIVITAT4/exercici1.py
@@ -1,10 +1,10 @@
 '''
-(A)Trucar a l’exercici 1 i mostrar per pantalla:
-La matriu com la de la imatge de l’exercici 1
-La dimensió de la matriu
-El tamany de la matriu
-Número total d’elements
-Tipus d’elements interns
+Trucar a l’exercici 1 i mostrar per pantalla:
+1. La matriu com la de la imatge de l’exercici 1
+2. La dimensió de la matriu
+3. El tamany de la matriu
+4. Número total d’elements
+5. Tipus d’elements interns
 
 Cada punt ha de vindre acompanyat d’un text on quedi clar 
 què s’està mostrant per pantalla
@@ -19,19 +19,16 @@ Guardar la ndarray en un arxiu de nom exercici1.npy.
 import numpy as np
 
 def crear_ndarray():
-    #crea una matriu de zeros de 50x50
-    matriu = np.zeros((50, 50), dtype = int)
+    #crear un ndarray amb numeros ascendents la diagonal de 0 a 49
+    arr = np.diag(np.arange(50))
 
-    #asigna numeros ascendents a la diagonal
-    for i in range(50):
-        matriu[i][i] = i
+    print("La matriu es: ")
+    print(arr)
+    
+    print("La dimensio de la matriu es: ", arr.shape)
+    print("El tamany de la matriu en bytes es: ", arr.nbytes)
+    print("El numero total d'elements de la matriu es: ", arr.size)
+    print("El tipus d'elements interns es: ",arr.dtype)
 
-    #guarda la matriu a un archiu .npy
-    np.save('exercici1.npy', matriu) 
-
-if __name__=="__main__":
-    crear_ndarray()       
-
-
-
-
+    #trucar
+    crear_ndarray()
