c6ae713c0531f8c9710f647f46037e29ebc0792a
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-14 18:44:05 +0200

    Exercici 2

---
+++ ACTIVITAT4/exercici2.py
@@ -0,0 +1,35 @@
+'''
+(B)Trucar a l’exercici 2 i mostrar per pantalla:
+Les matrius com les de la imatge d l’exercici 2.
+Número total d’elements
+La dimensió de la matriu
+Tipus d’elements interns
+El tamany de la matriu
+
+
+Una funció per crear una array com la de la imatge:
+[88, 23, 39, 41]
+
+Una funció per crear una array com la de la imatge:
+[[76.4, 21.7, 38.4], [41.2, 52.8, 68.9]]
+
+Una funció per crear una array com la de la imatge:
+12
+4
+9
+8
+'''
+#array
+array = [88, 23, 39, 41]
+print(array)
+
+#array bidimensional
+def array_bidimensional():
+    array = [[76.4, 21.7, 38.4], [41.2, 52.8, 68.9]]
+    return array
+
+resultat = array_bidimensional()
+print(resultat)
+
+
+#
