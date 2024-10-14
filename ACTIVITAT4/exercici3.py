ff35e6a74b10bddeecc1fc7b50303030c7482fc9
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-14 18:44:26 +0200

    Exercici 4

---
+++ ACTIVITAT4/exercici3.py
@@ -0,0 +1,20 @@
+'''
+(C) Trucar a l’exercici 3 i mostrar per pantalla:
+La matriu indicada per l’usuari.
+El valor màxim.
+El valor mínim.
+
+Cada punt ha de vindre acompanyat d’un text on quedi clar 
+què s’està mostrant per pantalla.
+
+Dintre d’un arxiu de nom exercici3.py tindrà les següent funcions:
+1.Una funció que generi una matriu amb números random de 0 a 100. 
+La dimensió (reshape) d’aquesta matriu se li demana a l’usuari per consola.
+    1.1 Mostrar per consola la matriu.
+    1.2 Redimensionar la matriu.
+    1.3 Mostrar per consola aquesta nova matriu redimensionada.
+2.Una funció que retorni el valor màxim de la matriu.
+3.Una funció que retorni el valor mínim de la matriu.
+
+
+'''
