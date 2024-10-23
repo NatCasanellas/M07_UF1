f40027db54b034c73299d03d431e3789161b191f
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-10-23 19:57:40 +0200

    commit

---
+++ ACTIVITAT5/ExerciciA.py
@@ -0,0 +1,22 @@
+'''
+Amb el paquet PANDAS i MATPLOTLIB crear 4 funcions que:
+
+1 funció que mostri per pantalla, la quantitat de casos total per mes per país (agafar 10 països) (agafar 4 mesos).
+1 funció que mostri per pantalla, les morts totals per mes per país (agafar 10 països) (agafar 4 mesos) del 2021.
+1 funció que mostri per pantalla la “reproduction_rate” per mes per país (agafar 10 països) (agafar 4 mesos).
+1 funció main la qual truqui a les 3 funcions i mostri, utilitzant matplotlib, 1 gràfica de línies per cada funció mostrant els resultats. (Cal que la gràfica tingui llegenda)
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Funcio 1:
+
+
+#Funcio 2:
+
+
+#Funcio 3:
+
+
+#Funcio main:
---
+++ ACTIVITAT5/ExerciciB.py
@@ -0,0 +1,20 @@
+'''
+Amb el paquet PANDAS i MATPLOTLIB crear 4 funcions que:
+		(Caldrà agafar 10 ciutats)
+1 funció que mostri per pantalla, el total de població per ciutat.
+1 funció que mostri per pantalla, la densitat per KM2 per ciutat.
+1 funció que mostri per pantalla, la densitat per M2 per ciutat.
+1 funció main la qual truqui a les 3 funcions i mostri, utilitzant matplotlib, 1 gràfica circular per cada funció mostrant els resultats. (Cal que la gràfica tingui llegenda i cal mostrar el % en cada “quesito”). Si veieu que en aquest format, la llegenda es sobreposa, agafeu una mostra des del main.
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#Funcio 1:
+
+#Funcio 2:
+
+#Funcio 3:
+
+#Funcio main:
